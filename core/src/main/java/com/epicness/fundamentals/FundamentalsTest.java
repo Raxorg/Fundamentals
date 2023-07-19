8b9800c3673580407d1c3117d6fe45892ce77a8a
Raxorg <lrdolucho@gmail.com>
2023-07-18 22:20:36 -0400

    Input handling upgrade

--- core/src/main/java/com/epicness/fundamentals/FundamentalsTest.java
+++ core/src/main/java/com/epicness/fundamentals/FundamentalsTest.java
@@ -13,9 +13,9 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.epicness.fundamentals.stuff.DualSprited;
+import com.epicness.fundamentals.utils.TextUtils;
 
-// Use this as the entry point for your game by
-// renaming (refactor) this class to your game's class
+// A Game subclass like this one (not this one) should be the entry point for your game
 public class FundamentalsTest extends Game {
 
     SpriteBatch spriteBatch;
@@ -28,7 +28,7 @@ public class FundamentalsTest extends Game {
         font = new BitmapFont(Gdx.files.internal("fundamentals/fonts/pixelFont.fnt"));
         font.getData().setScale(4f);
         Sprite a = new Sprite(new Texture(WEIRDSHAPE_SPRITE.fileName));
-        x = new DualSprited(a,a);
+        x = new DualSprited(a, a);
         x.setSize(100f);
     }
 
@@ -36,7 +36,7 @@ public class FundamentalsTest extends Game {
     public void render() {
         ScreenUtils.clear(Color.FOREST);
         spriteBatch.begin();
-        font.draw(spriteBatch, "A:B,C.D", 100f, 100f);
+        font.draw(spriteBatch, "F:U,N.D A\"M", 100f, 100f + TextUtils.getTextHeight(font, "F:U,N.D A\"M"));
         x.draw(spriteBatch);
         if (Gdx.input.isKeyPressed(K)) {
             x.stretchWidth(10f);
