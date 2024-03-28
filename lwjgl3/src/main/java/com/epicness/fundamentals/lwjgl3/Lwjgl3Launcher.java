19de8dadb56ae5cc8ca46e9d132f0b5af5d3b183
Groxar <lrdolucho@gmail.com>
2024-03-28 01:14:14 -0400

    Cleaned LWJGL3 launcher

--- lwjgl3/src/main/java/com/epicness/fundamentals/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/epicness/fundamentals/lwjgl3/Lwjgl3Launcher.java
@@ -4,15 +4,11 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.epicness.fundamentals.FundamentalsTest;
 
-/** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
         if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
-        createApplication();
-    }
 
-    private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new FundamentalsTest(), getDefaultConfiguration());
+        new Lwjgl3Application(new FundamentalsTest(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
@@ -24,7 +20,7 @@ public class Lwjgl3Launcher {
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-        configuration.setWindowedMode(640, 480);
+        configuration.setWindowedMode(1200, 700);
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
     }
